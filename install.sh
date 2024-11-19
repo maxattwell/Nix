d3ea4c9e4c52b40ef219456253be77a951050a04
Max Attwell <max.attwell@hotmail.com>
2024-11-19 14:01:17 +1300

    add install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+src="$HOME/nix-dots"
+if [ "$linux_os" = "nixos" ]
+then
+    dst="/etc/nixos"
+    sudo ln -sfn "$src/flake.nix" "$dst/flake.nix"
+    sudo ln -sfn "$src/flake.lock" "$dst/flake.lock"
+    sudo ln -sfn "$src/hosts" "$dst/hosts"
+    sudo ln -sfn "$src/modules" "$dst/modules"
