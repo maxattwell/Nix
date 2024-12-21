780b875d652b2179dbb928d762bcc1b14ebd53ec
Max Attwell <max.attwell@hotmail.com>
2024-12-22 11:29:31 +1300

    update soncarr insecure pkgs

--- modules/nixos/nixarr.nix
+++ modules/nixos/nixarr.nix
@@ -2,12 +2,16 @@
 
 {
   # Needed to build sonarr in 24.11
-  nixpkgs.config.permittedInsecurePackages = [
-    "aspnetcore-runtime-6.0.36"
-    "aspnetcore-runtime-wrapped-6.0.36"
-    "dotnet-sdk-6.0.428"
-    "dotnet-sdk-wrapped-6.0.428"
-  ];
+  # nixpkgs.config.permittedInsecurePackages = [
+    # "aspnetcore-runtime-6.0.36"
+    # "aspnetcore-runtime-wrapped-6.0.36"
+    # "dotnet-sdk-6.0.428"
+    # "dotnet-sdk-wrapped-6.0.428"
+  # ];
+    nixpkgs.config.permittedInsecurePackages = [
+      "aspnetcore-runtime-6.0.36"
+      "dotnet-sdk-6.0.428"
+    ];
 
   # Enable nixarr server
   nixarr = {
