4b83a749506d0f45ced1aafca28897721fef774f
Max Attwell <max.attwell@hotmail.com>
2024-12-22 00:02:20 +1300

    fix: add sonarr fix

--- hosts/shedservarr/default.nix
+++ hosts/shedservarr/default.nix
@@ -8,4 +8,8 @@
     ../../modules/nixos/homepage.nix
     ../../modules/nixos/kodi.nix
   ];
+
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
 }
--- modules/nixos/nixarr.nix
+++ modules/nixos/nixarr.nix
@@ -1,6 +1,14 @@
 { config, lib, pkgs, ... }:
 
 {
+  # Needed to build sonarr in 24.11
+  nixpkgs.config.permittedInsecurePackages = [
+    "aspnetcore-runtime-6.0.36"
+    "aspnetcore-runtime-wrapped-6.0.36"
+    "dotnet-sdk-6.0.428"
+    "dotnet-sdk-wrapped-6.0.428"
+  ];
+
   # Enable nixarr server
   nixarr = {
     enable = true;
