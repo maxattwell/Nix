3f97ae6628bde052f53269888f74a6c7b751ee59
Max Attwell <max.attwell@hotmail.com>
2024-12-22 13:57:01 +1300

    remove sonarr

--- modules/nixos/nixarr.nix
+++ modules/nixos/nixarr.nix
@@ -1,17 +1,10 @@
 { config, lib, pkgs, ... }:
 
 {
-  # Needed to build sonarr in 24.11
   # nixpkgs.config.permittedInsecurePackages = [
-    # "aspnetcore-runtime-6.0.36"
-    # "aspnetcore-runtime-wrapped-6.0.36"
-    # "dotnet-sdk-6.0.428"
-    # "dotnet-sdk-wrapped-6.0.428"
+  #   "aspnetcore-runtime-6.0.36"
+  #   "dotnet-sdk-6.0.428"
   # ];
-    nixpkgs.config.permittedInsecurePackages = [
-      "aspnetcore-runtime-6.0.36"
-      "dotnet-sdk-6.0.428"
-    ];
 
   # Enable nixarr server
   nixarr = {
@@ -21,7 +14,7 @@
     bazarr.enable = true;
     prowlarr.enable = true;
     radarr.enable = true;
-    sonarr.enable = true;
+    # sonarr.enable = true;
     readarr.enable = true;
   };
 }
