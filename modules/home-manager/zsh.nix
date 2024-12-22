f80fd147579c7b03e5a2975baf7e700ce2c46f03
Max Attwell <max.attwell@hotmail.com>
2024-12-22 17:49:41 +1300

    update homemanager zsh

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -9,17 +9,5 @@
     enableCompletion = true;
     history.size = 10000;
 
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "macos" ];
-    };
-
-    initExtra = ''
-      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-      ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#757575'
-      alias ls="${pkgs.eza}/bin/eza --icons=always --color=always"
-      alias finder="ofd"
-    '';
   };
 }
