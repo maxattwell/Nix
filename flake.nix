38102d37e4a44265d4fb343756481ccbaf169664
Max Attwell <max.attwell@hotmail.com>
2025-01-12 17:39:59 +1300

    feat: add homebrew

--- flake.nix
+++ flake.nix
@@ -1,24 +1,5 @@
-# Directory structure:
-# .
-# ├── flake.nix
-# ├── hosts
-# │   ├── macbookair
-# │   │   ├── default.nix
-# │   │   ├── system.nix
-# │   │   └── home.nix
-# │   └── shedservarr
-# │       ├── default.nix
-# │       └── hardware.nix
-# ├── modules
-# │   ├── darwin
-# │   │   ├── yabai.nix
-# │   │   └── skhd.nix
-# │   └── home-manager
-# │       └── zsh.nix
-
-# flake.nix
 {
-  description = "Nix systems configuration flake";
+  description = "Max Attwell Nix systems configuration flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -35,13 +16,23 @@
       url = "github:c4710n/nix-darwin-emacs";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
     nixarr = {
       url = "github:rasmus-kirk/nixarr";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nix-darwin, darwin-emacs, nixarr, ... }@inputs:
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    nix-darwin,
+    darwin-emacs,
+    nix-homebrew,
+    nixarr,
+    ...
+  }@inputs:
     let
       darwinSystem = "aarch64-darwin";
       linuxSystem = "x86_64-linux";
@@ -51,7 +42,9 @@
           system = linuxSystem;
           modules = [
             ./hosts/shedservarr
+
             nixarr.nixosModules.default
+
             home-manager.nixosModules.home-manager {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
@@ -67,7 +60,17 @@
           system = darwinSystem;
           modules = [
             ./hosts/macbookair
+
             { nixpkgs.overlays = [ darwin-emacs.overlays.emacs ]; }
+
+            nix-homebrew.darwinModules.nix-homebrew {
+              nix-homebrew = {
+                enable = true;
+                enableRosetta = true;
+                user = "max";
+              };
+            }
+
             home-manager.darwinModules.home-manager {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
