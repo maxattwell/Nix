043c5cf0c310990bce675a410ba901221b833239
Max Attwell <max.attwell@hotmail.com>
2025-04-12 21:36:28 +1200

    add nixbox home.nix

---
+++ hosts/nixbox/home.nix
@@ -0,0 +1,15 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    ../../modules/home-manager
+  ];
+
+  home = {
+    username = "max";
+    homeDirectory = "/home/max";
+    stateVersion = "24.11";
+  };
+
+  programs.home-manager.enable = true;
+}
