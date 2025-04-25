2e1d30db1b163e47cce26c75bb6171fc8ab852d7
Max Attwell <max.attwell@hotmail.com>
2025-04-25 21:36:05 +1200

    update doom ai and

--- modules/home-manager/waybar/default.nix
+++ modules/home-manager/waybar/default.nix
@@ -9,51 +9,57 @@
         enable = true;
 
         settings = {
-        mainBar = {
-            position = "bottom";
-            height = 30;
-            spacing = 20;
-            modules-left = ["hyprland/workspaces"];
-            modules-center = ["bluetooth" "custom/radioplayer"];
-            modules-right = ["memory" "cpu" "clock"];
-            "custom/radioplayer" = {
-                format = "";
-                return-type = "json";
-                on-click = "$HOME/Nix/assets/bin/play-radio.sh";
-                tooltip = false;
-            };
-            "hyprland/workspaces" = {
-                format = "{icon} <small>{windows}</small>";
-                window-rewrite-default = " ";
-                window-rewrite = {
-                    kitty = " ";
-                    chrome = " ";
-                    emacs = " ";
-                    GitHub = " ";
-                    Outlook = " ";
-                    WhatsApp = " ";
-                    "class<chrome> title<*.github.*>" = " ";
+            mainBar = {
+                position = "bottom";
+                height = 30;
+                spacing = 20;
+                modules-left = ["hyprland/workspaces"];
+                modules-center = ["bluetooth" "custom/radioplayer"];
+                modules-right = ["memory" "cpu" "custom/gpu" "clock"];
+                "custom/radioplayer" = {
+                    format = "";
+                    return-type = "json";
+                    on-click = "$HOME/Nix/assets/bin/play-radio.sh";
+                    tooltip = false;
+                };
+                "hyprland/workspaces" = {
+                    format = "{icon} <small>{windows}</small>";
+                    window-rewrite-default = " ";
+                    window-rewrite = {
+                        kitty = " ";
+                        chrome = " ";
+                        emacs = " ";
+                        GitHub = " ";
+                        Outlook = " ";
+                        WhatsApp = " ";
+                        "class<chrome> title<*.github.*>" = " ";
+                    };
+                };
+                bluetooth = {
+                    format = "󰂯";
+                    format-disabled = "󰂲";
+                    format-connected = "{num_connections} ";
+                    on-click = "blueman-manager";
+                };
+                clock = {
+                    timezone = "Pacific/Auckland";
+                    format = "<small>󰃭</small> {:%a %d %b     <small></small>  %I:%M  %p}";
+                    tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+                };
+                cpu = {
+                    format = "{usage}% ";
+                    tooltip = false;
+                };
+                memory = {
+                    format = "{}% ";
+                };
+                "custom/gpu" = {
+                    exec = "nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{print $1\"%\"}'"; 
+                    format = "{} 󰢮";
+                    interval = 5;
+                    tooltip = false;
                 };
             };
-            bluetooth = {
-                format = "󰂯";
-                format-disabled = "󰂲";
-                format-connected = "{num_connections} ";
-                on-click = "blueman-manager";
-            };
-            clock = {
-                timezone = "Pacific/Auckland";
-                format = "<small>󰃭</small> {:%a %d %b     <small></small>  %I:%M  %p}";
-                tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
-            };
-            cpu = {
-                format = "{usage}% ";
-                tooltip = false;
-            };
-            memory = {
-                format = "{}% ";
-            };
-        };
         };
     };
 }
