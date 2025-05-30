6fa3fbf86c087d4d3798b3cba63c987335708040
Max Attwell <max.attwell@hotmail.com>
2025-05-30 19:19:12 +1200

    move direnv to homemanager

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,61 +1,84 @@
 { config, lib, pkgs, ... }:
 
 {
-  programs.git = {
-    enable = true;
-    userName  = "Max Attwell";
-    userEmail = "max.attwell@hotmail.com";
-    extraConfig.init.defaultBranch = "main";
-    delta = {
+  programs = {
+
+    git = {
       enable = true;
-      options = {
-        line-numbers = true;
+      userName  = "Max Attwell";
+      userEmail = "max.attwell@hotmail.com";
+      extraConfig.init.defaultBranch = "main";
+      delta = {
+        enable = true;
+        options = {
+          line-numbers = true;
+        };
       };
     };
-  };
 
-  programs.zsh = {
-    enable = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-    enableCompletion = true;
-    history.size = 10000;
-    shellAliases = {
-      cat = "bat";
-      ls = "ls --color";
-    };
-    zplug = {
+    zsh = {
       enable = true;
-      plugins = [
-        { name = "agkozak/zsh-z"; }
-      ];
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      enableCompletion = true;
+      history.size = 10000;
+      shellAliases = {
+        cat = "bat";
+      };
+      zplug = {
+        enable = true;
+        plugins = [
+          { name = "agkozak/zsh-z"; }
+        ];
+      };
+
+      initContent = ''
+      bindkey '^k' history-search-backward
+      bindkey '^j' history-search-forward
+      bindkey '^h' backward-word
+      bindkey '^l' forward-word
+      bindkey '^[[3;5~' backward-kill-word
+      # Highlight selected item in completion menu
+      zstyle ':completion:*' menu select
+
+      # Function to update rprompt
+      update_rprompt() {
+        rprompt_parts=""
+        if [[ -n "$IN_NIX_SHELL" ]]; then
+          rprompt_parts+="%F{cyan}[nix-dev]%f "
+        fi
+        if [[ -n "$DIRENV_DIR" ]]; then
+          rprompt_parts+="%F{yellow}[direnv]%f "
+        fi
+        RPROMPT="$rprompt_parts"
+      }
+
+      # Update rprompt on directory change and before each command
+      autoload -U add-zsh-hook
+      add-zsh-hook chpwd update_rprompt
+      add-zsh-hook precmd update_rprompt
+
+      # Initial update
+      update_rprompt
+
+      # Custom rebuild function
+      rebuild() {
+        if [[ "$(uname -s)" == "Darwin" ]]; then
+          sudo darwin-rebuild switch --flake $HOME/Nix
+          echo "Rebuild complete, reloading yabai..."
+          sudo yabai --load-sa && echo "Yabai reload success"
+        else
+          sudo nixos-rebuild switch --flake $HOME/Nix --impure
+        fi
+      }
+      '';
     };
-    initContent = ''
-    bindkey '^k' history-search-backward
-    # bindkey '^j' history-search-forward
-    bindkey '^h' backward-word
-    bindkey '^l' forward-word
-    bindkey '^[[3;5~' backward-kill-word
 
-    # Highlight selected item in completion menu
-    zstyle ':completion:*' menu select
+    bat.enable = true;
 
-    if [[ -n "$IN_NIX_SHELL" ]]; then
-      RPROMPT="%F{cyan}[nix-dev]%f"
-    fi
-
-    # Custom rebuild function
-    rebuild() {
-      if [[ "$(uname -s)" == "Darwin" ]]; then
-        sudo darwin-rebuild switch --flake $HOME/Nix
-        echo "Rebuild complete, reloading yabai..."
-        sudo yabai --load-sa && echo "Yabai reload success"
-      else
-        sudo nixos-rebuild switch --flake $HOME/Nix --impure
-      fi
-    }
-    '';
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
   };
-
-  programs.bat.enable = true;
 }
