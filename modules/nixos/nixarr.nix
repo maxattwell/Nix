c31bf062592194cc5fcf8914b97603320a499680
Max Attwell <max.attwell@hotmail.com>
2025-06-17 00:11:17 +1200

    feat(nixos): Add Jellyseerr service

--- modules/nixos/nixarr.nix
+++ modules/nixos/nixarr.nix
@@ -15,4 +15,12 @@
     radarr.enable = true;
     sonarr.enable = true;
   };
+
+  # Jellyseerr config
+  services.jellyseerr = {
+    enable = true;
+    port = 5055;
+    openFirewall = true;
+    package = pkgs.jellyseerr; # Use the unstable package if stable is not up-to-date
+  };
 }
