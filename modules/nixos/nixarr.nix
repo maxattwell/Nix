49e3f7b1ea0c50356f7ab75f9fd6bbd6ed10725e
Max Attwell <max.attwell@hotmail.com>
2025-09-07 09:33:21 +1200

    add readarr to nixarr config

--- modules/nixos/nixarr.nix
+++ modules/nixos/nixarr.nix
@@ -15,13 +15,6 @@
     prowlarr.enable = true;
     radarr.enable = true;
     sonarr.enable = true;
+    readarr.enable = true;
   };
-
-  # # Jellyseerr config
-  # services.jellyseerr = {
-  #   enable = true;
-  #   port = 5055;
-  #   openFirewall = true;
-  #   package = pkgs.jellyseerr; # Use the unstable package if stable is not up-to-date
-  # };
 }
