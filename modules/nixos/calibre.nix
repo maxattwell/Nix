f5e4769d35b3a49d1361201eecf3405dbcb4d2f2
Max Attwell <max.attwell@hotmail.com>
2025-09-07 02:21:16 +1200

    update claibre

--- modules/nixos/calibre.nix
+++ modules/nixos/calibre.nix
@@ -1,5 +1,15 @@
 { config, lib, pkgs, ... }:
 
 {
-  services.calibre-web.enable = true;
+  services.calibre-web = {
+    enable = true;
+    listen.ip = "0.0.0.0";
+    listen.port = 8083;
+    options = {
+      calibreLibrary = "/var/lib/calibre-web/library";
+      enableBookUploading = true;
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 8083 ];
 }
