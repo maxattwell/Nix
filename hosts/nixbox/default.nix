a569fffb858ad4b295b00b756a64e08c47682767
Max Attwell <max.attwell@hotmail.com>
2025-10-25 09:40:13 +1300

    auto login and waybar icon

--- hosts/nixbox/default.nix
+++ hosts/nixbox/default.nix
@@ -5,7 +5,6 @@
     ../../modules/nixos
     ../../modules/nixos/bluetooth.nix
     ../../modules/nixos/nvidia.nix
-    ../../modules/nixos/sddm.nix
     ../../modules/nixos/emacs.nix
     ../../modules/nixos/dev.nix
   ];
@@ -22,7 +21,16 @@
     nameserver 8.8.8.8
   '';
 
-  # services.displayManager.ly.enable = true;
+  # Auto-login configuration
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.hyprland}/bin/Hyprland";
+        user = "max";
+      };
+    };
+  };
 
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
