52ec37f81ad247be5bf8ec6cae0e3c92b54cbebc
Max Attwell <max.attwell@hotmail.com>
2026-02-08 14:15:48 +1300

    update calibre-web config

--- modules/nixos/calibre-web.nix
+++ modules/nixos/calibre-web.nix
@@ -3,13 +3,14 @@
 {
   services.calibre-web = {
     enable = true;
+    group = "media";
     listen = {
       ip = "0.0.0.0";
       port = 8083;
     };
 
     options = {
-      calibreLibrary = "/data/calibre-library";
+      calibreLibrary = "/data/media/library/books";
       enableBookUploading = true;
       enableBookConversion = true;
     };
@@ -17,4 +18,8 @@
 
   # Open firewall for web access
   networking.firewall.allowedTCPPorts = [ 8083 ];
+
+  # # Fix for calibre-web missing metadata.db (when new db location is not valid)
+  # Run the following command to download and set permissions for the db
+  # cd /data/media/library/books && sudo curl -L "https://github.com/janeczku/calibre-web/raw/master/library/metadata.db" -o metadata.db && sudo chown calibre-web:media metadata.db && sudo chmod 664 metadata.db
 }
