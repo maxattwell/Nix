7094b77ca538ee6be7916bf72c7d635da3c0b435
Max Attwell <max.attwell@hotmail.com>
2026-05-09 15:24:45 +1200

    feat: Enable Docker for shedservarr

--- hosts/shedservarr/default.nix
+++ hosts/shedservarr/default.nix
@@ -18,6 +18,12 @@
     ghostty.terminfo
   ];
 
+  virtualisation.docker = {
+    enable = true;
+  };
+
+  users.users.max.extraGroups = [ "docker" ];
+
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
