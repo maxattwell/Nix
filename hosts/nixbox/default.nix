558c0748035c716bfdd7beab64f457cc797c53c6
Max Attwell <max.attwell@hotmail.com>
2026-06-14 12:24:36 +1200

    refactor: clean functions

--- hosts/nixbox/default.nix
+++ hosts/nixbox/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, hostname, ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./hardware-configuration.nix
--- hosts/nixtop/default.nix
+++ hosts/nixtop/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, hostname, ... }:
+{ ... }:
 {
   imports = [
     ./hardware-configuration.nix
